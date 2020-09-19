48d7bb668d38ff66419093f5e5eda1c972bdb050
khermalik <khermalik@gmail.com>
2020-09-18 19:05:12 -0700

    wrote pseudo code

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// -----------------------------  Actors  ------------------------
+// Opponents: aliens & player
+// alien and captain both have spaceships that share certain dynamic properties.
+
+// -----------------------------  Differences in Actors  ------------------------
+// player's ship's properties have known starting values
+// alien's ship's properties have different random starting values with each ship's instance.
+
+// -----------------------------  Attack  ------------------------
+// player attacks first
+// both alien and player attack in the same way (generate a random number to determine whether or not the attack hits the opponent.) 
+// the battle is back and forth attacks until someone's hull <= 0.
+
+// -----------------------------  if alien hull <= 0  ------------------------
+// prompt user to retreat or fight next alien ship. (6 alien ships in all.)
+
+// -----------------------------  End Game  ------------------------
+// if player destroys all alien ships - Game Over - Player wins
+// if player is destroyed - Game Over - Aliens win
+// if player retreats - Game Over - Aliens win
+
